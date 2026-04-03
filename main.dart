56878e18292982b74ad8da9126a4b13f21d4710b
Akshit saroha <akshitsaroha00@gmail.com>
2026-04-03 14:18:47 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app.dart';
 import 'injection.dart';
 
 void main() async {
+  // Ensure Flutter bindings are initialized before calling native code
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Initialize dependencies
+  // Initialize P2P and Database dependencies
   await configureDependencies();
   
   runApp(const NearbyChatApp());
